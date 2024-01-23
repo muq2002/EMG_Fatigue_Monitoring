c74a0990edc7352b680da32ca959f9993ece45d3
Muqtada Jabbar <mktad200@gmail.com>
2024-01-23 21:46:59 +0300

    Labeling data

---
+++ main_classifer.m
@@ -0,0 +1,34 @@
+clear; clc;
+%% Preparing Data
+emg_data1 = readmatrix('data/subject01.csv');
+emg_data2 = readmatrix('data/subject02.csv');
+
+fs = 2000;
+
+trials_subject_1 = selective_feature(emg_data1, fs);
+trials_subject_2 = selective_feature(emg_data2, fs);
+
+%% Splitting Data and Labeling
+total_number_of_rows = length(trials_subject_1) + length(trials_subject_2);
+emg_data = zeros(total_number_of_rows, 3);
+
+% Make the data in table
+emg_data(1:length(trials_subject_1), 1:2) = [trials_subject_1];
+emg_data(length(trials_subject_1) + 1:end, 1:2) = [trials_subject_2]
+
+% Labeling Data as 1 = fatigue; 0 = normal
+half_of_emg1 = length(trials_subject_1) / 2;
+half_of_emg2 = length(trials_subject_2) / 2;
+
+emg_data(half_of_emg1:length(trials_subject_1) -1 , 3) = ones(half_of_emg1, 1);
+emg_data((half_of_emg2 + length(trials_subject_1) + 1): end, 3) ... 
+    = ones(half_of_emg2, 1);
+
+%% Build Model
+% In this section we build the machine learing model.
+% We are use very simple machine learning algorthims such as SVM or LDR
+% becuase the linear relationship between the features.
+
+
+
+
---
+++ selective_feature.m
@@ -0,0 +1,26 @@
+function out = selective_feature(emg_data, fs)
+    % Split into channels
+    channel_1 = emg_data(:,2);
+    channel_2 = emg_data(:,3);
+
+    % Segmentation
+    window_size = 2 * fs;
+    number_window = floor(length(emg_data) / window_size);
+
+    trials_channel_1 = slicing_windows(channel_1, window_size, number_window);
+    trials_channel_2 = slicing_windows(channel_2, window_size, number_window);
+    
+    % Selecte Features
+    features_channel_1 = zeros(number_window, 8);
+    features_channel_2 = zeros(number_window, 8);
+
+    % Calculate all the features of emg by `calculate_features()`
+    for selected_window = 1:number_window
+        features_channel_1(selected_window, :) =  ... 
+            calcalate_features(trials_channel_1(selected_window, :), fs);
+        features_channel_2(selected_window, :) =  ... 
+            calcalate_features(trials_channel_2(selected_window, :), fs);
+    end
+    % I select the index is 3 to RMS feature and index 4 to ZC feature
+    out = [features_channel_1(:,3:4); features_channel_2(:,3:4)];
+end
